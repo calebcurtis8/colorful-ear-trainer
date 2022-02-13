59ec6fa7d98a677fd51f2da9e09c1d2065bbd7db
Caleb Curtis <caleb@softlimit.com>
2022-02-13 15:50:33 -0500

    add build hash for cache busting

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,25 +1,27 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 
-if((process.env.NODE_ENV == 'production')){
-  process.env.SNOWPACK_PUBLIC_ANALYTICS = 'G-QPDJ6X371X';
+process.env.SNOWPACK_PUBLIC_BUILD_HASH = Date.now().toString(36)
+
+if ((process.env.NODE_ENV === 'production')) {
+  process.env.SNOWPACK_PUBLIC_ANALYTICS = 'G-QPDJ6X371X'
 } else {
-  process.env.SNOWPACK_PUBLIC_ANALYTICS = '';
+  process.env.SNOWPACK_PUBLIC_ANALYTICS = ''
 }
 
 module.exports = {
   mount: {
     public: '/',
-    src: '/_dist_',
+    src: '/_dist_'
   },
   buildOptions: {
-    baseUrl: './',
+    baseUrl: './'
   },
   devOptions: {
-    tailwindConfig: './tailwind.config.js',
+    tailwindConfig: './tailwind.config.js'
   },
   plugins: [
     '@snowpack/plugin-webpack',
     '@snowpack/plugin-postcss',
-    ['snowpack-plugin-ejs'],
-  ],
+    ['snowpack-plugin-ejs']
+  ]
 }
